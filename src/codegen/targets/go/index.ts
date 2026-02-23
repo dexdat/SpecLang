d7148597f145a50e70983079f170c8ba49af3bbc
totalwindupflightsystems <totalwindupflightsystems@gmail.com>
2026-02-23 10:08:21 -0500

    speclang: implement P3-007 Go type mappings [parent:30785c3]
    Co-authored-by: Alexis Okuwa <wojonstech@gmail.com>

---
+++ src/codegen/targets/go/index.ts
@@ -0,0 +1,10 @@
+/**
+ * SPECLANG-GENERATED: Go type mappings index
+ * Source: @speclang/codegen @block:go-types-index
+ */
+
+export * from './types';
+export * from './types_primitives';
+export * from './types_collections';
+export * from './types_generics';
+export * from './types_special';
